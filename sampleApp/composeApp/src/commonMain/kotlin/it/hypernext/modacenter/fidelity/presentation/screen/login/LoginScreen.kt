eb8448af785fb0dd7447385bed88e8b2e0231b38
giacomolavermicocca <giacomo.cervignano@gmail.com>
2024-10-26 22:09:05 +0200

    Logica keyboard hide - todo label adapt to width

--- sampleApp/composeApp/src/commonMain/kotlin/it/hypernext/modacenter/fidelity/presentation/screen/login/LoginScreen.kt
+++ sampleApp/composeApp/src/commonMain/kotlin/it/hypernext/modacenter/fidelity/presentation/screen/login/LoginScreen.kt
@@ -7,13 +7,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -23,6 +29,9 @@ import com.mmk.kmpauth.firebase.google.GoogleButtonUiContainerFirebase
 import com.mmk.kmpauth.firebase.phone.PhoneAuthContainer
 import com.mmk.kmpauth.uihelper.apple.AppleSignInButton
 import com.mmk.kmpauth.uihelper.google.GoogleSignInButton
+import it.hypernext.modacenter.fidelity.Res
+import it.hypernext.modacenter.fidelity.title_login
+import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
@@ -41,19 +50,41 @@ fun LoginScreen(
         onLoginSuccess()
     }
 
+    val focusRequester = remember { FocusRequester() }
+    val focusManager = LocalFocusManager.current
+
     Column(
-        modifier = Modifier.fillMaxSize().padding(horizontal = 16.dp, vertical = 16.dp),
+        modifier = Modifier.fillMaxSize().padding(horizontal = 16.dp, vertical = 16.dp)
+            .focusRequester(focusRequester),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Top,
+        verticalArrangement = Arrangement.Center,
     ) {
+//        Row(modifier = Modifier.height(100.dp)){
+//            ScrollText()
+//        }
+//        Row {
+//            Text(
+//                text = "Login",
+//                style = MaterialTheme.typography.titleLarge,
+//                fontStyle = FontStyle.Italic,
+//                textAlign = TextAlign.Center,
+//                modifier = Modifier.fillMaxWidth().padding(bottom = 14.dp),
+//                fontSize = 60.sp
+//            )
+//        }
+        Row{
+            Button(onClick = {focusManager.clearFocus()}){
+                Text(text = "Clear Focus")
+            }
+        }
         Row {
             Text(
-                text = "Login",
-                style = MaterialTheme.typography.titleLarge,
+                text = stringResource(Res.string.title_login),
+                style = MaterialTheme.typography.displayMedium,
                 fontStyle = FontStyle.Italic,
-                textAlign = TextAlign.Start,
-                modifier = Modifier.fillMaxWidth().padding(bottom = 16.dp),
-                fontSize = 100.sp
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth().padding(bottom = 14.dp),
+                fontSize = 100.sp,
             )
         }
         viewModelAppSettings.errorSignin.let { error ->
@@ -91,7 +122,6 @@ fun LoginScreen(
                 modifier = Modifier.fillMaxWidth(),
                 onResult = viewModelAppSettings.onFirebaseResult
             )
-
         }
     }
 }
